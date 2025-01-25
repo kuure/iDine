6e84269d182c4a60b0f97b9c079aeea2f1e25cb9
kuure <kuure@msu.edu>
2025-01-25 16:11:26 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by jlk on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
